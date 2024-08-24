9c8ef2bd610e366229755ea993f557893f29c4ae
Niklas <n.ruedebusch@web.de>
2024-08-24 23:16:57 +0200

    preload font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script>
   import "../app.css";
-  import { load } from "@fontsource-variable/dm-sans";
-
-  load({
-    display: "swap",
-  });
+  import "@fontsource-variable/dm-sans";
+  import dmSansWoff2 from "@fontsource-variable/dm-sans/files/dm-sans-latin-400-normal.woff2?url";
 </script>
 
 <svelte:head>
@@ -12,6 +9,13 @@
   <meta
     name="description"
     content="Crafting modern web solutions with Sveltekit, Tailwind, and Firebase. I'm Niklas Rüdebusch, a frontend developer from Münster, Germany. Explore my portfolio to see how I bring ideas to life through clean, efficient code."
+  />
+  <link
+    rel="preload"
+    as="font"
+    type="font/woff2"
+    href={dmSansWoff2}
+    crossorigin="anonymous"
   />
 </svelte:head>
 
