746ca5d4fb49c580fd96fe2d56681f2c4ee4dfcf
Niklas <n.ruedebusch@web.de>
2024-08-24 23:24:05 +0200

    meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
   <title>Niklas Rüdebusch | Frontend Developer - Modern Web Solutions</title>
   <meta
     name="description"
-    content="Crafting modern web solutions with Sveltekit, Tailwind, and Firebase. I'm Niklas Rüdebusch, a frontend developer from Münster, Germany. Explore my portfolio to see how I bring ideas to life through clean, efficient code."
+    content="Crafting modern web solutions with Sveltekit, Tailwind, and Firebase. Explore my portfolio to see how I bring ideas to life through clean, efficient code."
   />
   <link
     rel="preload"
