079e2b6097225648ac9ca7f075f0309ce39b0d2d
Niklas <n.ruedebusch@web.de>
2024-08-25 13:49:16 +0200

    fontsource

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
   import "../app.css";
   import "@fontsource-variable/dm-sans";
-  import dmSansWoff2 from "@fontsource-variable/dm-sans/files/dm-sans-latin-400-normal.woff2?url";
 </script>
 
 <svelte:head>
@@ -9,13 +8,6 @@
   <meta
     name="description"
     content="Crafting modern web solutions with Sveltekit, Tailwind, and Firebase. Explore my portfolio to see how I bring ideas to life through clean, efficient code."
-  />
-  <link
-    rel="preload"
-    as="font"
-    type="font/woff2"
-    href={dmSansWoff2}
-    crossorigin="anonymous"
   />
   <link rel="preload" as="image" href="/assets/hero.webp" type="image/webp" />
 </svelte:head>
