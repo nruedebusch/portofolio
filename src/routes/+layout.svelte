11820b3017b8b71b81d4c6965defe0a5eb51709f
Niklas <n.ruedebusch@web.de>
2024-08-25 14:19:48 +0200

    preload font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,30 @@
     content="Crafting modern web solutions with Sveltekit, Tailwind, and Firebase. Explore my portfolio to see how I bring ideas to life through clean, efficient code."
   />
   <link rel="preload" as="image" href="/assets/hero.webp" type="image/webp" />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/DMSANS-Regular.woff2"
+    type="font/woff2"
+  />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/DMSANS-Bold.woff2"
+    type="font/woff2"
+  />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/DMSANS-Italic.woff2"
+    type="font/woff2"
+  />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/DMSANS-Medium.woff2"
+    type="font/woff2"
+  />
 </svelte:head>
 
 <slot />
