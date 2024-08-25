70041d90257433edf667c0b85c1aa448868656e6
Niklas <n.ruedebusch@web.de>
2024-08-25 13:47:24 +0200

    fontsource

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
-  build: {
-    rollupOptions: {
-      external: [
-        "@fontsource-variable/dm-sans/files/dm-sans-latin-400-normal.woff2?url",
-        "@fontsource-variable/dm-sans",
-      ],
-    },
-  },
 });
